27f9cc464e883218e0bc5f0e8a72d6d1c6ca5bb3
MrKabalan04 <159710101+MrKabalan04@users.noreply.github.com>
2026-01-15 18:29:45 +0200

    styling more and adding the api

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-/**
-Challenge: 
-
-- Add some styling! Be creative, play with layout a bit
-  (Spoiler: I'm going to use a single-column flexbox layout)
-  add some color, grab a font from Google fonts - whatever
-  you'd like!
-*/
-
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
+document.getElementById("get-activity").addEventListener("click", function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity").textContent = data.activity
+    })
+})
